30602a6c78b8ce606c32838cee4732a17d7d2f1a
Mel Sovereign <melsovereign@yahoo.com>
2016-09-19 08:39:15 -0400

    first commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+        Vector3 startDirection = Quaternion.Euler(0f, 0f, Random.Range(-65f, 65f)) * Vector3.right * 4f;
+        if (Random.value > .5f) {
+            startDirection *= -1f;
+        }
+        GetComponent<Rigidbody>().AddForce(startDirection, ForceMode.VelocityChange);
+    }
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+}
