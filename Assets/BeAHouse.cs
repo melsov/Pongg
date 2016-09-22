0af8e66627428b4ba3db87436c1250845a6d58ec
Mel Sovereign <melsovereign@yahoo.com>
2016-09-22 08:25:31 -0400

    Random house added

---
+++ Assets/BeAHouse.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeAHouse : MonoBehaviour {
+
+    public ParticleSystem smoke;
+
+    public void toggleSmoke() {
+        smoke.gameObject.SetActive(!smoke.gameObject.activeSelf);
+    }
+}
